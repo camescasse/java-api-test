7e7c3cfb7fcb1d3400a1eec4bd9e5d060f77f577
Eric Camescasse <eric.camescasse@flashparking.com>
2024-04-15 12:36:10 -0400

    added put, patch to api client and posts tests

--- src/main/java/org/example/services/PostService.java
+++ src/main/java/org/example/services/PostService.java
@@ -11,6 +11,10 @@ public class PostService {
         client = new APIClient<>("/posts", Post.class);
     }
 
+    public Post create(Post post) {
+        return client.post(post);
+    }
+
     public List<Post> getAll() {
         return client.getAll();
     }
@@ -19,8 +23,12 @@ public class PostService {
         return client.getById(id);
     }
 
-    public Post create(Post post) {
-        return client.post(post);
+    public Post override(Post post, int id) {
+        return client.put(post, id);
+    }
+
+    public Post update(Post post, int id) {
+        return client.patch(post, id);
     }
 
     public int delete(int id) {
